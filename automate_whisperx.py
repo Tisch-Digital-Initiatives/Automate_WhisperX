1a020595a566437275b9b6c67dc8cd8fdd68a060
hayley-curtis-4 <hayley.curtis@tufts.edu>
2025-09-30 10:54:37 -0400

    Update automate_whisperx.py
    Fixed input/output argument bug

--- automate_whisperx.py
+++ automate_whisperx.py
@@ -26,19 +26,20 @@ if __name__=="__main__":
     parser.add_argument("-i", "--indirectory", help="Input directory containing the A/V files.")
     parser.add_argument("-o", "--outdirectory", help="Output directory where A/V files + generated transcripts are moved to.")
     args = parser.parse_args()
-    if args.indirectory or args.outdirectory is None: 
-        print("please include -i and -o arguments to specify input/output directories!")
-        exit()
-    elif args.indirectory == args.outdirectory:
-        print("Input/Output directories must be different from each other!")
-        exit()
-    elif not os.path.exists(args.indirectory):
-        print("The input directory does not exist!")
-        exit()
-    elif not os.path.exists(args.outdirectory):
-        print("The output directory does not exist!")
-        exit()
-    process_files(args.indirectory, args.outdirectory)
+    if args.indirectory and args.outdirectory is not None: 
+        if args.indirectory == args.outdirectory:
+            print("Input/Output directories must be different from each other!")
+            exit()
+        elif not os.path.exists(args.indirectory):
+            print("The input directory does not exist!")
+            exit()
+        elif not os.path.exists(args.outdirectory):
+            print("The output directory does not exist!")
+            exit()
+        process_files(args.indirectory, args.outdirectory)
+    else: 
+        print("please run this program with -i and -o arguments")
 #Runs script, requires input/output directories as arguments, checks to make sure directories actually exist
     
+
 
